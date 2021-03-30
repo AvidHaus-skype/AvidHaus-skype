a26dade11aaf9ec722e1657790bebc3956974348
Max <Max@bizzwc.local>
2021-03-31 02:02:02 +0500

    group Created

--- src/Components/ChatWindow/AddChat/GroupListContainer/GroupListContainer.js
+++ src/Components/ChatWindow/AddChat/GroupListContainer/GroupListContainer.js
@@ -4,7 +4,7 @@ import { Button, Input, Checkbox, Avatar } from "@material-ui/core";
 import "./groupListContainer.css";
 import axios from "axios";
 import { useSelector } from "react-redux";
-function GroupMemebers({ members }) {
+function GroupMemebers({ members, userId, setUserId }) {
   const [selectMember, setSelectMember] = useState(false);
 
   //   if (selectMember) {
@@ -16,6 +16,7 @@ function GroupMemebers({ members }) {
       className="groupMembers"
       onClick={() => {
         setSelectMember(!selectMember);
+        setUserId([...userId, members.user_id]);
       }}
     >
       <div className="groupMemberInfo">
@@ -43,6 +44,7 @@ function GroupListContainer({
 }) {
   const [groupMember, setGroupMember] = useState("");
   const [memberList, setMemberList] = useState([]);
+  const [userId, setUserId] = useState([]);
 
   const data = useSelector((state) => {
     return state;
@@ -62,6 +64,32 @@ function GroupListContainer({
       });
   }, []);
 
+  const HandleGroup = () => {
+    const imgData = new FormData();
+    imgData.append("group_image", passGroupPicture);
+    axios
+      .post("http://192.168.0.96:401/bwccrm/createGroup", {
+        loginuser_id: data.user.data.user_id,
+        user_id: data.user.data.user_id,
+        group_name: passGroupName,
+        imgData,
+        members: userId,
+      })
+      .then((res) => {
+        console.log(res);
+        alert(res.data.message);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    console.log({
+      loginuser_id: data.user.data.user_id,
+      user_id: data.user.data.user_id,
+      group_name: passGroupName,
+      group_image: data,
+      members: userId,
+    });
+  };
   return (
     <div className="groupListContainer">
       <div className="groupListContainer__header">
@@ -77,8 +105,7 @@ function GroupListContainer({
           onClick={() => {
             setGroupModelName(false);
             setgroupModelListContaier(false);
-            console.log(passGroupPicture);
-            console.log(passGroupName);
+            HandleGroup();
           }}
         >
           Done
@@ -94,8 +121,13 @@ function GroupListContainer({
         />
       </div>
       <div className="groupListContainer__memberList">
-        {memberList.map((members) => (
-          <GroupMemebers members={members} />
+        {memberList.map((members, id) => (
+          <GroupMemebers
+            members={members}
+            userId={userId}
+            setUserId={setUserId}
+            key={id}
+          />
         ))}
       </div>
     </div>
--- src/Components/MessageWindow/MessageInput/MessageInput.js
+++ src/Components/MessageWindow/MessageInput/MessageInput.js
@@ -4,27 +4,30 @@ import SendIcon from "@material-ui/icons/Send";
 import AttachmentIcon from "@material-ui/icons/Attachment";
 import { Button, Input } from "@material-ui/core";
 import axios from "axios";
-import {useSelector} from 'react-redux'
-
+import { useSelector } from "react-redux";
 
 function MessageInput() {
   const [message, setMessage] = useState("");
   const [attach, setAttach] = useState();
-  const data=useSelector(state=>{return state})
+  const data = useSelector((state) => {
+    return state;
+  });
 
   // console.log(data.chat)
-  console.log(data.chat)
+  console.log(data.chat);
 
-  const SendMessage=()=>{
-    return axios.post('http://192.168.0.96:401/bwccrm/sendMessage',{
-      user_id:data.user.data.user_id,
-      loginuser_id: data.user.data.user_id,
-      message_to: data.chat.user_id,
-      message_body: message
-    }).then(res=>{
-      console.log(res)
-    })
-  }
+  const SendMessage = () => {
+    return axios
+      .post("http://192.168.0.96:401/bwccrm/sendMessage", {
+        user_id: data.user.data.user_id,
+        loginuser_id: data.user.data.user_id,
+        message_to: data.chat.user_id,
+        message_body: message,
+      })
+      .then((res) => {
+        console.log(res);
+      });
+  };
 
   const getAttchment = () => {};
   return (

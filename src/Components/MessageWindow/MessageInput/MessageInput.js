173d60ebc9c702c4c7e31ef71054983127656fa6
unknown <Max@DESIGNER3-PC.bizzwc.local>
2021-04-01 03:43:03 +0500

    'finalpush'

--- src/Components/MessageWindow/MessageInput/MessageInput.js
+++ src/Components/MessageWindow/MessageInput/MessageInput.js
@@ -5,31 +5,14 @@ import AttachmentIcon from "@material-ui/icons/Attachment";
 import { Button } from "@material-ui/core";
 import axios from "axios";
 import { useSelector } from "react-redux";
-import Pusher from 'pusher-js';
-<script src="https://js.pusher.com/7.0/pusher.min.js" />
+
 
 function MessageInput() {
   const [message, setMessage] = useState("");
   const [pushermessage, setpushermessage] = useState("");
   const [attach, setAttach] = useState();
   const data=useSelector(state=>{return state})
-    useEffect(()=>{
-      var pusher = new Pusher('f30ce11a6ce537110adc', {
-        cluster: 'ap2',
-        useTLS : true
-      });
-      
-      Pusher.logToConsole = true;
-      
-      var channel = pusher.subscribe('bwccrm-chat');
-          channel.bind('messaging', function(data) {
-            // alert(JSON.stringify(data));
-            const message_array =message
-            message_array.push(data)
-            setMessage(message_array)
-          });
-      
-    },[message])
+    
 
   const SendMessage = () => {
     return axios
--- src/Components/MessageWindow/MessageTextContainer/MessageTextContainer.js
+++ src/Components/MessageWindow/MessageTextContainer/MessageTextContainer.js
@@ -3,14 +3,33 @@ import "./MessageTextContainer.css";
 import UserMessage from "./UserMessage/UserMessage";
 import axios from "axios";
 import { useSelector } from "react-redux";
+import Pusher from 'pusher-js';
+<script src="https://js.pusher.com/7.0/pusher.min.js" />
 
 function MessageTextContainer() {
   const data = useSelector((state) => {
     return state;
   });
   const [chattingdata, setchattingdata] = useState([]);
+  const [message, setMessage] = useState("");
+
 
   useEffect(() => {
+    var pusher = new Pusher('f30ce11a6ce537110adc', {
+      cluster: 'ap2',
+      useTLS : true
+    });
+    
+    Pusher.logToConsole = true;
+    
+    var channel = pusher.subscribe('bwccrm-chat');
+        channel.bind('messaging', function(data) {
+          // alert(JSON.stringify(data));
+          const message_array =chattingdata
+          message_array.push(data)
+          setchattingdata(message_array)
+        });
+        
     axios
       .post("http://192.168.0.96:401/bwccrm/fetchMessage", {
         from_id: data.Auth.data.user_id,
